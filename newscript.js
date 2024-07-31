bf82fb5024315eb90fbf4f2883e3a58b25625818
unknown <akhilachandranck91@gmail.com>
2024-07-31 20:21:07 +0530

    first

---
+++ newscript.js
@@ -0,0 +1,78 @@
+console.log('My first code');
+// Variables
+var a=5
+console.log(a);
+let b=10;
+{
+    var a=9;
+    console.log(a)
+}
+let car=new Object()
+car.name='Alto'
+car.colour='red'
+console.log(car);
+function display(fname)
+{
+console.log('Welcome'+fname+' to function');
+
+
+}
+//display('Akhila');
+function mult_value(a,b){
+var product1=a*b;
+return product1;
+
+
+}
+var result1=mult_value(10,20)
+console.log('the product is' +result1)
+var exp= (4*7)-8**2+9;
+console.log(exp);
+if(exp>0)
+{
+console.log('Value is greater than zero');
+
+}else if(exp==0){
+
+    console.log('Value is equal to zero');
+
+}
+else{
+
+    console.log('Value is less than zero');
+
+}
+for( let i=0;i<10;i++)
+    {
+    console.log(i+1);
+    
+    }
+    var array=[1,45,34,56,78];
+    for(let i=0;i<array.length;i++)
+    {
+    console.log(array[i]);
+    
+    }
+    var obj={fname:'akhila',age:30,location:'pandalam'}
+    for (const i in obj
+    
+    ) {
+        console.log(i);
+    }
+    for (const i of array) {
+        console.log(i);
+    
+        
+    }
+    let i=0;
+    while(i<10)
+    {
+        console.log(i+1);
+        i++;
+    }
+    let r=1;
+    do {
+        console.log(r);
+        r++;
+    } while (r<=10);
+
