8d3265ff3e76d7e2a3b52a634ef0129d2236cb4b
Akhilachandran-ck <akhilachandranck91@gmail.com>
2024-08-03 06:28:26 +0530

    sum of square

--- prime1.js
+++ prime1.js
@@ -1,7 +1,7 @@
 function array1()
 
 {
-var arrr1=[15,56,9,91];
+var arrr1=[11,56,9,91];
 console.log(arrr1[0]);
 var j=0;
 for(let i=1; i<=arrr1[0]; i++)
---
+++ square.js
@@ -0,0 +1,11 @@
+var arr1=[3,2,1,5,7];
+var sq=0;
+var sum=0;
+for(let i=0;i<arr1.length;i++)
+{
+    sq=arr1[i]*arr1[i];
+    sum=sum+sq;
+
+}
+console.log(sum);
+
